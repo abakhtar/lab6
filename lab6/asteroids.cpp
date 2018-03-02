bb5ea3e06ff756b0502d27ece6b425b1559d6e54
Student <student@lab-315-032.cs.csubak.edu>
2018-03-02 15:02:33 -0800

    added my individual file

--- lab6/asteroids.cpp
+++ lab6/asteroids.cpp
@@ -746,6 +746,10 @@ void render()
 	ggprint8b(&r, 16, 0x00ffff00, "n bullets: %i", g.nbullets);
 	ggprint8b(&r, 16, 0x00ffff00, "n asteroids: %i", g.nasteroids);
 	ggprint8b(&r, 16, 0x00ffff00, "n asteroids destroyed: ");
+	extern void showName();
+	showName();
+	extern void drawBox(int x, int y);
+	drawBox(gl.xres/2, gl.yres/2);
 	//
 	//-------------
 	//Draw the ship
